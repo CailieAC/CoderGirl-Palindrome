b7c07ff6475055cc773a7979365d85f7be59db2c
Carlile Upfront <carlileupfront@users.noreply.github.com>
2019-03-09 23:20:55 -0600

    commit1

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -6,6 +6,10 @@ namespace Palindrome
     {
         public static void Main()
         {
+            Console.Write("Enter a word to check for palindrome: ");
+            string userInput = Console.ReadLine();
+            Console.WriteLine(IsPalindrome(userInput));
+
             Console.ReadLine();
         }
 
@@ -13,5 +17,19 @@ namespace Palindrome
         // The name of the method you create should be IsPalindrome
         // The method should take a string as the input.
         // The method should return a bool - true if the input is a palindrome and false if it is not.
+        public static bool IsPalindrome(string userInput)
+        {
+            bool isWordPalindrome = false;
+
+            char[] inputArray = userInput.ToCharArray();
+            
+            Array.Reverse(inputArray);
+            string reversedUserInput = String.Concat(inputArray);
+
+            if (String.Equals(userInput.ToLower(), reversedUserInput.ToLower()))
+                isWordPalindrome = true;
+
+            return isWordPalindrome;
+        }
     }
 }
